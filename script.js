fbf82e0f0d4c081d9947bc018d4b741ca2a2041f
motorinpg <motorinpg@63pokupki.ru>
2022-12-23 21:48:23 +0400

    naslajdaemsia i mashem

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
+// Закомментировать код ниже если нужно будет отключить радужный фон к
 setInterval(() => {
     document.body.style.transitionDuration = '2s';
     const o = Math.round;
-    const r = Math.random;
+    let r = Math.random;
     const s = 255;
-    const color = 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+    let color = 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
     document.body.style.background = color;
 }, 1000);
+// Закомментировать код выше если нужно будет отключить радужный фон ккк
 
 document.getElementById('12345').addEventListener('click', () => {
     alert('Андрей гей')
 })
 
 document.getElementById('1111').addEventListener('click', () => {
-    const lalala = document.getElementById('22222').value;
+    let lalala = document.getElementById('22222').value;
     const lalala2 = document.getElementById('333333');
-    const lalala4 = lalala2.options[lalala2.selectedIndex].text;
+    let lalala4 = lalala2.options[lalala2.selectedIndex].text;
     const lalala3 = document.getElementById('444444').value;
 
     const div = document.createElement('div');
     div.innerHTML = `<i class="right"></i> <i class="right"></i> ${lalala3} ${lalala4} ${lalala}</div>`;
-    const hehe = document.getElementById('fast-reg')
+    let hehe = document.getElementById('fast-reg')
     hehe.append(div);
+})
+
+document.getElementById('banan').addEventListener('click', () => {
+    const lololoshka = document.getElementById('s2tep-box').value;
+
+    const div = document.createElement('div');
+    div.innerHTML = `<li>
+    ${lololoshka}
+</li>`;
+    let hoho = document.getElementById('kukuruza')
+    hoho.append(div);
 })
